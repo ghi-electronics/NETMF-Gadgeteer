3f34dcb87e515c09ffa4040fe31ea2aa08bfc5a9
john_brochue <john.brochue@ghielectronics.com>
2013-07-11 15:32:15 -0400

    NET-24 #resolve Added ReflectorR3 tester and driver.

---
+++ Modules/GHIElectronics/Reflector R3/TestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Presentation.Shapes;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+
+namespace TestApp
+{
+	public partial class Program
+	{
+		// This method is run when the mainboard is powered up or reset.   
+		void ProgramStarted()
+		{
+			var reflector = new Gadgeteer.Modules.GHIElectronics.Reflector_R3(2);
+			new Thread(() =>
+			{
+				while (true)
+				{
+					Debug.Print("Left: " + reflector.Read(Gadgeteer.Modules.GHIElectronics.Reflector_R3.Reflectors.Left).ToString("F3") + " Center: " + reflector.Read(Gadgeteer.Modules.GHIElectronics.Reflector_R3.Reflectors.Center).ToString("F3") + " Right: " + reflector.Read(Gadgeteer.Modules.GHIElectronics.Reflector_R3.Reflectors.Right).ToString("F3"));
+					Thread.Sleep(500);
+				}
+			}).Start();
+		}
+	}
+}
