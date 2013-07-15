c7103db8902eba0c371da0d4aa47c818bf37319e
john_brochue <john.brochue@ghielectronics.com>
2013-07-15 13:54:37 -0400

    NET-23 #comment Started progress on the DAQ driver, still no response from the device.

---
+++ Modules/GHIElectronics/DAQ 8B/TestApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Controls;
+using Microsoft.SPOT.Presentation.Media;
+using Microsoft.SPOT.Presentation.Shapes;
+using Microsoft.SPOT.Touch;
+
+using Gadgeteer.Networking;
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+
+namespace TestApp
+{
+	public partial class Program
+	{
+		void ProgramStarted()
+		{
+			var atd = new GTM.GHIElectronics.DAQ_8B(6);
+			new Thread(() =>
+			{
+				string result;
+				while (true)
+				{
+					result = atd.GetReading(GTM.GHIElectronics.DAQ_8B.Channel.P1).ToString() + " ";
+					result += atd.GetReading(GTM.GHIElectronics.DAQ_8B.Channel.P2).ToString() + " ";
+					result += atd.GetReading(GTM.GHIElectronics.DAQ_8B.Channel.P3).ToString() + " ";
+					result += atd.GetReading(GTM.GHIElectronics.DAQ_8B.Channel.P4).ToString() + " ";
+					result += atd.GetReading(GTM.GHIElectronics.DAQ_8B.Channel.P5).ToString() + " ";
+					result += atd.GetReading(GTM.GHIElectronics.DAQ_8B.Channel.P6).ToString() + " ";
+					result += atd.GetReading(GTM.GHIElectronics.DAQ_8B.Channel.P7).ToString() + " ";
+					result += atd.GetReading(GTM.GHIElectronics.DAQ_8B.Channel.P8).ToString();
+
+					Debug.Print(result);
+
+					Thread.Sleep(250);
+				}
+			}).Start();
+		}
+	}
+}
