26d52314ce5a8162c14a645e0e1c686c68ea1a3b
john_brochue <john.brochue@ghielectronics.com>
2013-09-18 10:25:10 -0400

    NET-38 Initial ButtonS7 driver. Still needs testing, image, and power and dimension measurements.

---
+++ Modules/GHIElectronics/ButtonS7/TestApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+
+using GTM = Gadgeteer.Modules;
+
+namespace TestApp
+{
+	public partial class Program
+	{
+		void ProgramStarted()
+		{
+			this.ButtonS7.EnterPressed += (sender, state) => { Debug.Print("Enter Pressed"); };
+			this.ButtonS7.EnterReleased += (sender, state) => { Debug.Print("Enter Released"); };
+
+			new Thread(() =>
+			{
+				while (true)
+				{
+					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Back)) Debug.Print("Back");
+					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Forward)) Debug.Print("Forward");
+					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Left)) Debug.Print("Left");
+					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Right)) Debug.Print("Right");
+					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Up)) Debug.Print("Up");
+					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Down)) Debug.Print("Down");
+					Thread.Sleep(50);
+				}
+			}).Start();
+		}
+	}
+}
