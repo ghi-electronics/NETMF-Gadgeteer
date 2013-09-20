5de7dff5b0429b1eac13ab09266796bc81b05dd0
john_brochue <john.brochue@ghielectronics.com>
2013-09-20 12:55:33 -0400

    Tweaked ButtonS7 test app.

--- Modules/GHIElectronics/ButtonS7/TestApp/Program.cs
+++ Modules/GHIElectronics/ButtonS7/TestApp/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.SPOT;
 
 using GTM = Gadgeteer.Modules;
+using Gadgeteer.Modules.GHIElectronics;
 
 namespace TestApp
 {
@@ -9,19 +10,19 @@ namespace TestApp
 	{
 		void ProgramStarted()
 		{
-			this.ButtonS7.EnterPressed += (sender, state) => { Debug.Print("Enter Pressed"); };
-			this.ButtonS7.EnterReleased += (sender, state) => { Debug.Print("Enter Released"); };
-
+			this.buttonS7.EnterPressed += (sender, state) => { Debug.Print("Enter Pressed"); };
+			this.buttonS7.EnterReleased += (sender, state) => { Debug.Print("Enter Released"); };
+			
 			new Thread(() =>
 			{
 				while (true)
 				{
-					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Back)) Debug.Print("Back");
-					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Forward)) Debug.Print("Forward");
-					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Left)) Debug.Print("Left");
-					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Right)) Debug.Print("Right");
-					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Up)) Debug.Print("Up");
-					if (this.ButtonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Down)) Debug.Print("Down");
+					if (this.buttonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Back)) Debug.Print("Back");
+					if (this.buttonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Forward)) Debug.Print("Forward");
+					if (this.buttonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Left)) Debug.Print("Left");
+					if (this.buttonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Right)) Debug.Print("Right");
+					if (this.buttonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Up)) Debug.Print("Up");
+					if (this.buttonS7.IsPressed(GTM.GHIElectronics.ButtonS7.Buttons.Down)) Debug.Print("Down");
 					Thread.Sleep(50);
 				}
 			}).Start();
