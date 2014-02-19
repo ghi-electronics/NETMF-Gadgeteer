a24204b7c560c465a4264c13245b734b26f8d0f0
John <john.brochue@ghielectronics.com>
2014-02-19 14:14:53 -0500

    Added G400HDR and G120HDR mainboards.

---
+++ Mainboards/GHIElectronics/G120HDR/G120HDR_43/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.Gadgeteer.G120HDR")]
+[assembly: AssemblyDescription("NETMF 4.3 driver for G120HDR mainboard made by GHI Electronics for Microsoft .NET Gadgeteer")]
+[assembly: AssemblyProduct("G120HDR")]
---
+++ Mainboards/GHIElectronics/G400HDRBreakout/G400HDRBreakout_43/G400HDRBreakout_43.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+using GT = Gadgeteer;
+using GTM = Gadgeteer.Modules;
+
+namespace GHIElectronics.Gadgeteer
+{
+    /// <summary>
+    /// Support class for GHI Electronics TemplateMainboard for Microsoft .NET Gadgeteer
+    /// </summary>
+    public class G400HDRBreakout : GT.Mainboard
+    {
+        // The mainboard constructor gets called before anything else in Gadgeteer (module constructors, etc), 
+        // so it can set up fields in Gadgeteer.dll specifying socket types supported, etc.
+
+        /// <summary>
+        /// Instantiates a new TemplateMainboard mainboard
+        /// </summary>
+        public G400HDRBreakout()
+        {
+            // comment the following if you do not support NativeI2C for faster DaisyLink performance
+            // otherwise, the DaisyLink I2C interface will be supported in Gadgeteer.dll in managed code.
+            GT.SocketInterfaces.I2CBusIndirector nativeI2C = (s, sdaPin, sclPin, address, clockRateKHz, module) => new InteropI2CBus(s, sdaPin, sclPin, address, clockRateKHz, module);
+            GT.Socket socket;
+
+            // For each socket on the mainboard, create, configure and register a Socket object with Gadgeteer.dll
+            // This specifies:
+            // - the SupportedTypes character array matching the list on the mainboard
+            // - the CpuPins array (indexes [3] to [9].  [1,2,10] are constant (3.3V, 5V, GND) and [0] is unused.  This is normally based on an enumeration supplied in the NETMF port used.
+            // - for other functionality, e.g. UART, SPI, etc, properties in the Socket class are set as appropriate to enable Gadgeteer.dll to access this functionality.
+            // See the Mainboard Builder's Guide and specifically the Socket Types specification for more details
+            // The two examples below are not realistically implementable sockets, but illustrate how to initialize a wide range of socket functionality.
+
+            // This example socket 1 supports many types
+            // Type 'D' - no additional action
+            // Type 'I' - I2C pins must be used for the correct CpuPins
+            // Type 'K' and 'U' - UART pins and UART handshaking pins must be used for the correct CpuPins, and the SerialPortName property must be set.
+            // Type 'S' - SPI pins must be used for the correct CpuPins, and the SPIModule property must be set 
+            // Type 'X' - the I2CBusIndirector is set (though by default "nativeI2C" is null) 
+            socket = GT.Socket.SocketInterfaces.CreateNumberedSocket(1);
+            socket.SupportedTypes = new char[] { 'D', 'I', 'K', 'S', 'U', 'X' };
+            socket.CpuPins[3] = (Cpu.Pin)1;
+            socket.CpuPins[4] = (Cpu.Pin)52;
+            socket.CpuPins[5] = (Cpu.Pin)23;
+            socket.CpuPins[6] = (Cpu.Pin)12;
+            socket.CpuPins[7] = (Cpu.Pin)34;
+            socket.CpuPins[8] = (Cpu.Pin)5;
+            socket.CpuPins[9] = (Cpu.Pin)7;
+            socket.I2CBusIndirector = nativeI2C;
+            socket.SerialPortName = "COM1";
+            socket.SPIModule = SPI.SPI_module.SPI1;
+            GT.Socket.SocketInterfaces.RegisterSocket(socket);
+
+            // This example socket 2 supports many types
+            // Type 'A' - AnalogInput3-5 properties are set and GT.Socket.SocketInterfaces.SetAnalogInputFactors call is made
+            // Type 'O' - AnalogOutput property is set
+            // Type 'P' - PWM7-9 properties are set
+            // Type 'Y' - the NativeI2CWriteRead function pointer is set (though by default "nativeI2C" is null) 
+            socket = GT.Socket.SocketInterfaces.CreateNumberedSocket(2);
+            socket.SupportedTypes = new char[] { 'A', 'O', 'P', 'Y' };
+            socket.CpuPins[3] = (Cpu.Pin)11;
+            socket.CpuPins[4] = (Cpu.Pin)5;
+            socket.CpuPins[5] = (Cpu.Pin)3;
+            socket.CpuPins[6] = (Cpu.Pin)66;
+            // Pin 7 not connected on this socket, so it is left unspecified
+            socket.CpuPins[8] = (Cpu.Pin)59;
+            socket.CpuPins[9] = (Cpu.Pin)18;
+            socket.AnalogOutput5 = Cpu.AnalogOutputChannel.ANALOG_OUTPUT_0;
+            GT.Socket.SocketInterfaces.SetAnalogInputFactors(socket, 1, 2, 10);
+            socket.AnalogInput3 = Cpu.AnalogChannel.ANALOG_2;
+            socket.AnalogInput4 = Cpu.AnalogChannel.ANALOG_3;
+            socket.AnalogInput5 = Cpu.AnalogChannel.ANALOG_1;
+            socket.PWM7 = Cpu.PWMChannel.PWM_3;
+            socket.PWM8 = Cpu.PWMChannel.PWM_0;
+            socket.PWM9 = Cpu.PWMChannel.PWM_2;
+            socket.I2CBusIndirector = nativeI2C;
+            GT.Socket.SocketInterfaces.RegisterSocket(socket);
+        }
+
+        private class InteropI2CBus : GT.SocketInterfaces.I2CBus
+        {
+            public override ushort Address { get; set; }
+            public override int Timeout { get; set; }
+            public override int ClockRateKHz { get; set; }
+
+            private Cpu.Pin sdaPin;
+            private Cpu.Pin sclPin;
+
+            public InteropI2CBus(GT.Socket socket, GT.Socket.Pin sdaPin, GT.Socket.Pin sclPin, ushort address, int clockRateKHz, GTM.Module module)
+            {
+                this.sdaPin = socket.CpuPins[(int)sdaPin];
+                this.sclPin = socket.CpuPins[(int)sclPin];
+                this.Address = address;
+                this.ClockRateKHz = clockRateKHz;
+            }
+
+            public override void WriteRead(byte[] writeBuffer, int writeOffset, int writeLength, byte[] readBuffer, int readOffset, int readLength, out int numWritten, out int numRead)
+            {
+                // implement this method if you support NativeI2CWriteRead for faster DaisyLink performance
+                // otherwise, the DaisyLink I2C interface will be supported in Gadgeteer.dll in managed code. 
+                numRead = 0;
+                numWritten = 0;
+                return;
+            }
+        }
+
+        private static string[] sdVolumes = new string[] { "SD" };
+
+        /// <summary>
+        /// Allows mainboards to support storage device mounting/umounting.  This provides modules with a list of storage device volume names supported by the mainboard. 
+        /// </summary>
+        public override string[] GetStorageDeviceVolumeNames()
+        {
+            return sdVolumes;
+        }
+
+        /// <summary>
+        /// Functionality provided by mainboard to mount storage devices, given the volume name of the storage device (see <see cref="GetStorageDeviceVolumeNames"/>).
+        /// This should result in a <see cref="Microsoft.SPOT.IO.RemovableMedia.Insert"/> event if successful.
+        /// </summary>
+        public override bool MountStorageDevice(string volumeName)
+        {
+            // implement this if you support storage devices. This should result in a <see cref="Microsoft.SPOT.IO.RemovableMedia.Insert"/> event if successful and return true if the volumeName is supported.
+            return volumeName == "SD";
+        }
+
+        /// <summary>
+        /// Functionality provided by mainboard to ummount storage devices, given the volume name of the storage device (see <see cref="GetStorageDeviceVolumeNames"/>).
+        /// This should result in a <see cref="Microsoft.SPOT.IO.RemovableMedia.Eject"/> event if successful.
+        /// </summary>
+        public override bool UnmountStorageDevice(string volumeName)
+        {
+            // implement this if you support storage devices. This should result in a <see cref="Microsoft.SPOT.IO.RemovableMedia.Eject"/> event if successful and return true if the volumeName is supported.
+            return volumeName == "SD";
+        }
+
+        /// <summary>
+        /// Changes the programming interafces to the one specified.
+        /// </summary>
+        /// <param name="programmingInterface">The programming interface to use</param>
+        public override void SetProgrammingMode(GT.Mainboard.ProgrammingInterface programmingInterface)
+        {
+            // Change the reflashing interface to the one specified, if possible.
+            // This is an advanced API that we don't expect people to call much.
+        }
+
+        /// <summary>
+        /// Configure the onboard display controller to fulfil the requirements of a display using the RGB sockets.
+        /// If doing this requires rebooting, then the method must reboot and not return.
+        /// If there is no onboard display controller, then NotSupportedException must be thrown.
+        /// </summary>
+        /// <param name="displayModel">Display model name.</param>
+        /// <param name="width">Display physical width in pixels, ignoring the orientation setting.</param>
+        /// <param name="height">Display physical height in lines, ignoring the orientation setting.</param>
+        /// <param name="orientationDeg">Display orientation in degrees.</param>
+        /// <param name="timing">The required timings from an LCD controller.</param>
+        protected override void OnOnboardControllerDisplayConnected(string displayModel, int width, int height, int orientationDeg, GT.Modules.Module.DisplayModule.TimingRequirements timing)
+        {
+            throw new NotSupportedException("This mainboard does not support an onboard display controller.");
+        }
+
+        /// <summary>
+        /// Called when the onboard display controller's display is disconnected, so any resources used by the onboard display controller could be reclaimed. 
+        /// </summary>
+        protected override void OnOnboardControllerDisplayDisconnected()
+        {
+            // it is optional to do anything with this method
+        }
+
+        /// <summary>
+        /// Ensures that the pins on R, G and B sockets (which also have other socket types) are available for use for non-display purposes.
+        /// If doing this requires rebooting, then the method must reboot and not return.
+        /// If there is no onboard display controller, or it is not possible to disable the onboard display controller, then NotSupportedException must be thrown.
+        /// </summary>
+        public override void EnsureRgbSocketPinsAvailable()
+        {
+            throw new NotSupportedException("This mainboard does not support an onboard display controller.");
+        }
+
+        // change the below to the debug led pin on this mainboard
+        private const Cpu.Pin DebugLedPin = Cpu.Pin.GPIO_NONE;
+
+        private Microsoft.SPOT.Hardware.OutputPort debugLed;
+        /// <summary>
+        /// Turns the debug LED on or off.
+        /// </summary>
+        /// <param name="on">True if the debug LED should be on</param>
+        public override void SetDebugLED(bool on)
+        {
+            if (debugLed == null)
+            {
+                if (DebugLedPin == Cpu.Pin.GPIO_NONE) return;
+                debugLed = new OutputPort(DebugLedPin, false);
+            }
+
+            debugLed.Write(on);
+        }
+
+        /// <summary>
+        /// This performs post-initialization tasks for the mainboard.  It is called by Gadgeteer.Program.Run and does not need to be called manually.
+        /// </summary>
+        public override void PostInit()
+        {
+            return;
+        }
+
+        /// <summary>
+        /// The mainboard name, which is printed at startup in the debug window
+        /// </summary>
+        public override string MainboardName
+        {
+            get { return "GHI Electronics TemplateMainboard"; }
+        }
+
+        /// <summary>
+        /// The mainboard version, which is printed at startup in the debug window
+        /// </summary>
+        public override string MainboardVersion
+        {
+            get { return "1.0"; }
+        }
+
+    }
+}
