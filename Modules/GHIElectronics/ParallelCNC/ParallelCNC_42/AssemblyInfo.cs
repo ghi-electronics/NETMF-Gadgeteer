4996352f942d61a3492b3aa8aeaa3789bfc4eea3
John <john.brochue@ghielectronics.com>
2014-02-19 13:37:03 -0500

    Added some old modules back.

---
+++ Modules/GHIElectronics/ParallelCNC/ParallelCNC_42/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GTM.GHIElectronics.ParallelCNC")]
+[assembly: AssemblyDescription("Driver for ParallelCNC module made by GHI Electronics for Microsoft .NET Gadgeteer")]
+[assembly: AssemblyProduct("ParallelCNC")]
