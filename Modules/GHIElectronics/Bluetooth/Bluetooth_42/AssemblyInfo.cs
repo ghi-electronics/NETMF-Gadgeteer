9830d2575cbf815dda536ade0d71cc3c059ebdb4
John <john.brochue@ghielectronics.com>
2014-03-25 13:09:18 -0400

    Removed Bluetooth module.

--- Modules/GHIElectronics/Bluetooth/Bluetooth_42/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GTM.GHIElectronics.Bluetooth")]
-[assembly: AssemblyDescription("Driver for Bluetooth module made by GHI Electronics for Microsoft .NET Gadgeteer")]
-[assembly: AssemblyProduct("Bluetooth")]
