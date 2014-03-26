f5b96109cf87a2c562e2f7bbe42e0a52dab76cd7
John <john.brochue@ghielectronics.com>
2014-03-26 11:23:00 -0400

    Removed some discontinued modules.

--- Modules/GHIElectronics/DisplayT35/DisplayT35_42/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GTM.GHIElectronics.DisplayT35")]
-[assembly: AssemblyDescription("Driver for DisplayT35 module made by GHI Electronics for Microsoft .NET Gadgeteer")]
-[assembly: AssemblyProduct("DisplayT35")]
--- Modules/GHIElectronics/PIR/PIR_42/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GTM.GHIElectronics.PIR")]
-[assembly: AssemblyDescription("Driver for PIR module made by GHI Electronics for Microsoft .NET Gadgeteer")]
-[assembly: AssemblyProduct("PIR")]
