edb1bd0a24b9018f5c4af505ce441c7d6e0fabef
John <john.brochue@ghielectronics.com>
2014-04-17 09:17:19 -0400

    Improved the RS485 and RS232.

--- Modules/GHIElectronics/RS485/RS485_43/RS485_43.cs
+++ Modules/GHIElectronics/RS485/RS485_43/RS485_43.cs
@@ -1,49 +1,60 @@
-﻿using GT = Gadgeteer;
+﻿using System;
+using GT = Gadgeteer;
 using GTI = Gadgeteer.SocketInterfaces;
 using GTM = Gadgeteer.Modules;
 
 namespace Gadgeteer.Modules.GHIElectronics
 {
-	/// <summary>
-	/// A RS485 module for Microsoft .NET Gadgeteer
-	/// </summary>
-	public class RS485 : GTM.Module
-	{
-		private GTI.Serial port;
-		private GT.Socket socket;
+    /// <summary>
+    /// An RS485 module for Microsoft .NET Gadgeteer
+    /// </summary>
+    public class RS485 : GTM.Module
+    {
+        private GT.SocketInterfaces.Serial serialPort;
+        private Socket socket;
 
-		/// <summary>Constructs a new RS485 instance.</summary>
-		/// <param name="socketNumber">The socket that this module is plugged in to.</param>
-		public RS485(int socketNumber)
-		{
-			this.socket = Socket.GetSocket(socketNumber, true, this, null);
-
-			this.socket.EnsureTypeIsSupported('U', this);
-		}
-
-        /// <summary>
-        /// Initializes the serial port to the passed in parameters. If no parameters are passed, the defaults are used.
-        /// </summary>
-        /// <param name="baudRate">The baud rate for the serail port. Defaulted to 38400.</param>
-        /// <param name="parity">Specifies the parity bit for the serial port. Defaulted to none.</param>
-        /// <param name="stopBits">Specifies the number of stop bits used on the serial port. Defaulted to one.</param>
-        /// <param name="dataBits">The number of data bits. Defaulted to 8.</param>
-        public GTI.Serial Initialize(int baudRate = 38400, GTI.SerialParity parity = GTI.SerialParity.None, GTI.SerialStopBits stopBits = GTI.SerialStopBits.One, int dataBits = 8)
+        /// <summary>Constructs a new instance.</summary>
+        /// <param name="socketNumber">The socket that this module is plugged in to.</param>
+        public RS485(int socketNumber)
         {
-            this.port = GTI.SerialFactory.Create(this.socket, baudRate, parity, stopBits, dataBits, GTI.HardwareFlowControl.NotRequired, this);
-            this.port.Open();
-			return this.port;
+            this.socket = Socket.GetSocket(socketNumber, true, this, null);
+            this.socket.EnsureTypeIsSupported('U', this);
+            this.serialPort = null;
         }
 
-		/// <summary>
-		/// The serial port the module provides.
-		/// </summary>
-		public GTI.Serial Port
-		{
-			get
-			{
-				  return this.port;
-			}
-		}
-	}
+        /// <summary>
+        /// Initializes the serial port with the parameters 38400 baud, 8N1, with no flow control.
+        /// </summary>
+        public void Configure()
+        {
+            this.Configure(38400, GTI.SerialParity.None, GTI.SerialStopBits.One, 8, GTI.HardwareFlowControl.NotRequired);
+        }
+
+        /// <summary>
+        /// Initializes the serial port with the given parameters.
+        /// </summary>
+        /// <param name="baudRate">The baud rate to use.</param>
+        /// <param name="parity">The parity to use.</param>
+        /// <param name="stopBits">The stop bits to use.</param>
+        /// <param name="dataBits">The number of data bits to use.</param>
+        /// <param name="flowControl">The flow control to use.</param>
+        public void Configure(int baudRate, GTI.SerialParity parity, GTI.SerialStopBits stopBits, int dataBits, GTI.HardwareFlowControl flowControl)
+        {
+            this.serialPort = GTI.SerialFactory.Create(socket, baudRate, parity, stopBits, dataBits, flowControl, this);
+            this.serialPort.Open();
+        }
+
+        /// <summary>
+        /// The underlying serial port object.
+        /// </summary>
+        public GT.SocketInterfaces.Serial Port
+        {
+            get
+            {
+                if (this.serialPort == null) throw new InvalidOperationException("You must call Configure first.");
+
+                return this.serialPort;
+            }
+        }
+    }
 }
