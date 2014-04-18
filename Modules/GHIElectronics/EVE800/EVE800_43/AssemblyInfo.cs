8e69f3f3e4fa312648a811bb4d32c86db58770b8
John <john.brochue@ghielectronics.com>
2014-04-18 15:57:03 -0400

    Removed EVE800.

--- Modules/GHIElectronics/EVE800/EVE800_43/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GTM.GHIElectronics.EVE800")]
-[assembly: AssemblyDescription("Driver for EVE800 module made by GHI Electronics for Microsoft .NET Gadgeteer")]
-[assembly: AssemblyProduct("EVE800")]
