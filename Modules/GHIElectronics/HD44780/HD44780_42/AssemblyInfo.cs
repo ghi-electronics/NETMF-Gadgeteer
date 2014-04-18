6014ba59bbb5d1aac2ab93ef16ed88243ce221d9
John <john.brochue@ghielectronics.com>
2014-04-18 16:06:00 -0400

    Moved the CharacterDisplay into 4.2 instead of the HD44780.

--- Modules/GHIElectronics/HD44780/HD44780_42/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GTM.GHIElectronics.HD44780")]
-[assembly: AssemblyDescription("Driver for HD44780 module made by GHI Electronics for Microsoft .NET Gadgeteer")]
-[assembly: AssemblyProduct("HD44780")]
