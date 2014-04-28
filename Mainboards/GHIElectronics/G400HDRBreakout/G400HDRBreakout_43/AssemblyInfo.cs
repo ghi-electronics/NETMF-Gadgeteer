b6c45139b9c13d4fc7e805500c9f4332c12b63ec
John <john.brochue@ghielectronics.com>
2014-04-28 15:30:26 -0400

    Readded and implemented the G400HDR.

---
+++ Mainboards/GHIElectronics/G400HDRBreakout/G400HDRBreakout_43/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.Gadgeteer.G400HDRBreakout")]
+[assembly: AssemblyDescription("NETMF 4.3 driver for G400HDRBreakout mainboard made by GHI Electronics for Microsoft .NET Gadgeteer")]
+[assembly: AssemblyProduct("G400HDRBreakout")]
