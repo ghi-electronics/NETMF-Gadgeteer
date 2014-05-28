d56850570c5f831a71dd0ca62b754b48b875602c
John <john.brochue@ghielectronics.com>
2014-05-28 15:12:23 -0400

    Added DisplayN18 tester.

--- Modules/GHIElectronics/DisplayN18/DisplayN18_Tester/Program.cs
+++ Modules/GHIElectronics/DisplayN18/DisplayN18_Tester/Program.cs
@@ -1,27 +1,47 @@
-﻿using System;
-using System.Collections;
-using System.Threading;
-using Microsoft.SPOT;
-using Microsoft.SPOT.Presentation;
-using Microsoft.SPOT.Presentation.Controls;
-using Microsoft.SPOT.Presentation.Media;
-using Microsoft.SPOT.Presentation.Shapes;
-using Microsoft.SPOT.Touch;
+﻿using System.Threading;
 
-using Gadgeteer.Networking;
 using GT = Gadgeteer;
-using GTM = Gadgeteer.Modules;
-using Gadgeteer.Modules.GHIElectronics;
 
 namespace DisplayN18_Tester
 {
     public partial class Program
     {
+        private GT.Timer timer;
+        private int next;
+
         void ProgramStarted()
         {
             this.displayT43.SimpleGraphics.DisplayText("DisplayN18 Tester", Resources.GetFont(Resources.FontResources.small), GT.Color.White, 0, 0);
             Thread.Sleep(2000);
 
+            this.next = 0;
+            this.timer = new GT.Timer(750);
+            this.timer.Tick += (a) =>
+            {
+                this.displayT43.SimpleGraphics.Clear();
+                switch (this.next)
+                {
+                    case 0: this.Show(GT.Color.Red, "Red"); break;
+                    case 1: this.Show(GT.Color.Green, "Green"); break;
+                    case 2: this.Show(GT.Color.Blue, "Blue"); break;
+                    case 3: this.Show(GT.Color.White, "White"); break;
+                    case 4: this.Show(GT.Color.Black, "Black"); break;
+                    case 5: this.Show(GT.Color.Yellow, "Yellow"); break;
+                    case 6: this.Show(GT.Color.Purple, "Purple"); break;
+                    case 7: this.Show(GT.Color.Orange, "Orange"); break;
+                }
+
+                this.next = (++this.next) % 8;
+            };
+            this.timer.Start();
+        }
+
+        private void Show(GT.Color c, string s)
+        {
+            this.displayT43.SimpleGraphics.DisplayText("Screens " + s.ToLower(), Resources.GetFont(Resources.FontResources.small), GT.Color.White, 0, 0);
+            this.displayN181.SimpleGraphics.DisplayRectangle(c, 1, c, 0, 0, 128, 160);
+            this.displayN182.SimpleGraphics.DisplayRectangle(c, 1, c, 0, 0, 128, 160);
+            this.displayN183.SimpleGraphics.DisplayRectangle(c, 1, c, 0, 0, 128, 160);
         }
     }
 }
