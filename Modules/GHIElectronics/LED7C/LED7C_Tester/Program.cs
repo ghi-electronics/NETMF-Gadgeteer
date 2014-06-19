c47f633bf80f7ef5763f4da01171b5c3b973efcb
John <john.brochue@ghielectronics.com>
2014-06-19 13:03:06 -0400

    Added some testers.

--- Modules/GHIElectronics/LED7C/LED7C_Tester/Program.cs
+++ Modules/GHIElectronics/LED7C/LED7C_Tester/Program.cs
@@ -1,27 +1,47 @@
-﻿using System;
-using System.Collections;
+﻿using Gadgeteer.Modules.GHIElectronics;
 using System.Threading;
-using Microsoft.SPOT;
-using Microsoft.SPOT.Presentation;
-using Microsoft.SPOT.Presentation.Controls;
-using Microsoft.SPOT.Presentation.Media;
-using Microsoft.SPOT.Presentation.Shapes;
-using Microsoft.SPOT.Touch;
-
-using Gadgeteer.Networking;
 using GT = Gadgeteer;
-using GTM = Gadgeteer.Modules;
-using Gadgeteer.Modules.GHIElectronics;
 
 namespace LED7C_Tester
 {
     public partial class Program
     {
-        void ProgramStarted()
+        private GT.Timer timer;
+
+        private void ProgramStarted()
         {
             this.displayT43.SimpleGraphics.DisplayText("LED7C Tester", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 0);
             Thread.Sleep(2000);
 
+            this.timer = new GT.Timer(1000);
+            this.timer.Tick += (a) =>
+            {
+                this.displayT43.SimpleGraphics.Clear();
+
+                this.Set(LED7C.Color.Red, "red");
+                this.Set(LED7C.Color.Green, "green");
+                this.Set(LED7C.Color.Blue, "blue");
+                this.Set(LED7C.Color.White, "white");
+                this.Set(LED7C.Color.Off, "off");
+            };
+            this.timer.Start();
+        }
+
+        private void Set(LED7C.Color color, string colorString)
+        {
+            this.displayT43.SimpleGraphics.Clear();
+            this.displayT43.SimpleGraphics.DisplayText("LEDs are now " + colorString, Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 0);
+            this.led7C1.SetColor(color);
+            this.led7C2.SetColor(color);
+            this.led7C3.SetColor(color);
+            this.led7C4.SetColor(color);
+            this.led7C5.SetColor(color);
+            this.led7C6.SetColor(color);
+            this.led7C7.SetColor(color);
+            this.led7C8.SetColor(color);
+            this.led7C9.SetColor(color);
+            this.led7C10.SetColor(color);
+            Thread.Sleep(750);
         }
     }
 }
