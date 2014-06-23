3d44518d35ea59510d6d096bac3708718f66bba7
John <john.brochue@ghielectronics.com>
2014-06-23 15:35:41 -0400

    Moved KeypadKP16 and Barometer to the legacy SDK.

--- Modules/GHIElectronics/Barometer/Barometer_43/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GTM.GHIElectronics.Barometer")]
-[assembly: AssemblyDescription("Driver for Barometer module made by GHI Electronics for Microsoft .NET Gadgeteer")]
-[assembly: AssemblyProduct("Barometer")]
