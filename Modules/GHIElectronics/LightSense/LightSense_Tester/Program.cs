a92388ab8e4a9a294925aa75dc96700ce6f0ec35
John <john.brochue@ghielectronics.com>
2014-06-24 12:24:41 -0400

    Added LightSense tester.

--- Modules/GHIElectronics/LightSense/LightSense_Tester/Program.cs
+++ Modules/GHIElectronics/LightSense/LightSense_Tester/Program.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections;
-using System.Threading;
-using Microsoft.SPOT;
-using Microsoft.SPOT.Presentation;
-using Microsoft.SPOT.Presentation.Controls;
-using Microsoft.SPOT.Presentation.Media;
-using Microsoft.SPOT.Presentation.Shapes;
-using Microsoft.SPOT.Touch;
-
-using Gadgeteer.Networking;
+﻿using System.Threading;
 using GT = Gadgeteer;
-using GTM = Gadgeteer.Modules;
-using Gadgeteer.Modules.GHIElectronics;
 
 namespace LightSense_Tester
 {
     public partial class Program
     {
+        private GT.Timer timer;
+
         void ProgramStarted()
         {
             this.displayT43.SimpleGraphics.DisplayText("LightSense Tester", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 0);
             Thread.Sleep(2000);
 
+            this.timer = new GT.Timer(100);
+            this.timer.Tick += (a) =>
+                {
+                    this.displayT43.SimpleGraphics.Clear();
+
+                    this.displayT43.SimpleGraphics.DisplayText("Socket  2: " + (this.lightSense1.ReadProportion() * 100).ToString("f2"), Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 0);
+                    this.displayT43.SimpleGraphics.DisplayText("Socket 13: " + (this.lightSense2.ReadProportion() * 100).ToString("f2"), Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 15);
+                    this.displayT43.SimpleGraphics.DisplayText("Socket 18: " + (this.lightSense3.ReadProportion() * 100).ToString("f2"), Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 30);
+                };
+            this.timer.Start();
         }
     }
 }
