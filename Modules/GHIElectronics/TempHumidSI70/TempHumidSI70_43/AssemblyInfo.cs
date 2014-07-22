34102e891ea8ba08a6f7be8ffca13eb9eb90d9c1
John <john.brochue@ghielectronics.com>
2014-07-22 09:56:27 -0400

    Added TempHumidSI70.

---
+++ Modules/GHIElectronics/TempHumidSI70/TempHumidSI70_43/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GTM.GHIElectronics.TempHumidSI70")]
+[assembly: AssemblyDescription("Driver for TempHumidSI70 module made by GHI Electronics for Microsoft .NET Gadgeteer")]
+[assembly: AssemblyProduct("TempHumidSI70")]
