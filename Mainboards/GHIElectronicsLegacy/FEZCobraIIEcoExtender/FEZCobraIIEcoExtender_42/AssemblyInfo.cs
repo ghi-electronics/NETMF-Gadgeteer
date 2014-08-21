3a43eba0f8050606a35927702d41f45506255141
John <john.brochue@ghielectronics.com>
2014-08-21 15:13:33 -0400

    Changed the legacy folders to discontinued and removed the legacy mainboards.

--- Mainboards/GHIElectronicsLegacy/FEZCobraIIEcoExtender/FEZCobraIIEcoExtender_42/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("GHIElectronics.Gadgeteer.FEZCobraIIEcoExtender")]
-[assembly: AssemblyDescription("NETMF 4.3 driver for FEZCobraIIEcoExtender mainboard made by GHI Electronics for Microsoft .NET Gadgeteer")]
-[assembly: AssemblyProduct("FEZCobraIIEcoExtender")]
