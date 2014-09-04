1ada503de51feb388c73981cf4b5c1b3cbd8e79e
John <john.brochue@ghielectronics.com>
2014-09-04 16:09:15 -0400

    Added ENC28 tester.

--- Modules/GHIElectronics/EthernetENC28/EthernetENC28_Tester/Program.cs
+++ Modules/GHIElectronics/EthernetENC28/EthernetENC28_Tester/Program.cs
@@ -1,7 +1,6 @@
 ﻿using GHI.Networking;
 using Microsoft.SPOT;
 using Microsoft.SPOT.Net.NetworkInformation;
-using System;
 using System.Net;
 using System.Threading;
 using GT = Gadgeteer;
@@ -13,6 +12,7 @@ namespace EthernetENC28_Tester
         void ProgramStarted()
         {
             this.displayT43.SimpleGraphics.DisplayText("EthernetENC28 Tester", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 0);
+            Thread.Sleep(2000);
 
             new Thread(this.DoNetwork).Start();
         }
@@ -22,14 +22,19 @@ namespace EthernetENC28_Tester
             NetworkChange.NetworkAvailabilityChanged += (a, b) => Debug.Print("SPOT NAVAC " + b.IsAvailable.ToString());
             NetworkChange.NetworkAddressChanged += (a, b) => Debug.Print("SPOT NADRC");
             var socket = GT.Socket.GetSocket(1, true, null, null);
+            var result = new byte[8192 * 4];
+            var read = 0;
 
             while (true)
             {
-                Thread.Sleep(5000);
                 this.displayT43.SimpleGraphics.Clear();
-                this.displayT43.SimpleGraphics.DisplayText("Press LD0 to begin.", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 0);
+                this.displayT43.SimpleGraphics.DisplayText("Press LDR0 to begin.", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 0);
+
                 while (Mainboard.LDR0.Read())
-                    Thread.Sleep(250);
+                    Thread.Sleep(10);
+
+                this.displayT43.SimpleGraphics.Clear();
+                this.displayT43.SimpleGraphics.DisplayText("Testing.", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 0);
 
                 using (var netif = new EthernetENC28J60(socket.SPIModule, socket.CpuPins[6], socket.CpuPins[3], socket.CpuPins[4]))
                 {
@@ -43,48 +48,28 @@ namespace EthernetENC28_Tester
                         Thread.Sleep(250);
                     }
 
-                    Debug.Print(netif.IPAddress);
-
-                    byte[] result = new byte[8192 * 4];
-
-                    for (int i = 0; i < 10; i++)
+                    using (var req = HttpWebRequest.Create("http://www.bing.com/robots.txt") as HttpWebRequest)
                     {
-                        DateTime start, end;
-                        int read = 0, total = 0, count = 0;
+                        req.KeepAlive = false;
 
-                        using (var req = HttpWebRequest.Create("http://www.bing.com/robots.txt") as HttpWebRequest)
+                        using (var res = req.GetResponse() as HttpWebResponse)
                         {
-                            req.KeepAlive = false;
-                            start = DateTime.Now;
-
-                            using (var res = req.GetResponse() as HttpWebResponse)
+                            using (var stream = res.GetResponseStream())
                             {
-                                using (var stream = res.GetResponseStream())
+                                do
                                 {
-                                    do
-                                    {
-                                        read = stream.Read(result, 0, result.Length);
+                                    read = stream.Read(result, 0, result.Length);
 
-                                        total += read;
-                                        count++;
-
-                                        Thread.Sleep(20);
-                                    } while (read != 0);
-
-                                    end = DateTime.Now;
-                                }
+                                    Thread.Sleep(20);
+                                } while (read != 0);
                             }
                         }
-
-                        var s = end.AddMilliseconds(-20 * count) - start;
-                        var ms = s.Minutes * 60000 + s.Seconds * 1000 + s.Milliseconds;
-                        this.displayT43.SimpleGraphics.DisplayText(i.ToString() + " " + ((total / 1024.0) / (ms / 1000.0)).ToString("N2") + "    " + total.ToString() + "    " + ms.ToString(), Resources.GetFont(Resources.FontResources.NinaB), GT.Color.White, 0, 15);
-
-                        Thread.Sleep(100);
                     }
                 }
 
-                this.displayT43.SimpleGraphics.DisplayText("Module passes.", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.Green, 0, 30);
+                this.displayT43.SimpleGraphics.Clear();
+                this.displayT43.SimpleGraphics.DisplayText("Module passes.", Resources.GetFont(Resources.FontResources.NinaB), GT.Color.Green, 0, 0);
+                Thread.Sleep(2000);
             }
         }
     }
