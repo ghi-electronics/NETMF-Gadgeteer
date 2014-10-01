c8c9e1928a28c21dbaa7317fe0c4fdfebc923cab
John <john.brochue@ghielectronics.com>
2014-10-01 15:13:43 -0400

    Removed the msm part of the build as it is no longer used.

---
+++ Modules/GHIElectronicsDiscontinued/AssemblyInfoGlobal43.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("4.3.4.0")]
+[assembly: AssemblyFileVersion("4.3.4.0")]
+[assembly: AssemblyInformationalVersion("4.3.4.0")]
+
+[assembly: AssemblyCompany("GHI Electronics")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
