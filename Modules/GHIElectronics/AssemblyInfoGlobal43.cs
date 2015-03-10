35ed0823c2aa693a7e933c62259503b38b50bdc2
John <john.brochue@ghielectronics.com>
2015-03-10 13:22:08 -0400

    Updated the version.

--- Modules/GHIElectronics/AssemblyInfoGlobal43.cs
+++ Modules/GHIElectronics/AssemblyInfoGlobal43.cs
@@ -1,10 +1,10 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("4.3.7.1")]
-[assembly: AssemblyFileVersion("4.3.7.1")]
-[assembly: AssemblyInformationalVersion("4.3.7.1")]
+[assembly: AssemblyVersion("4.3.7.2")]
+[assembly: AssemblyFileVersion("4.3.7.2")]
+[assembly: AssemblyInformationalVersion("4.3.7.2")]
 
 [assembly: AssemblyCompany("GHI Electronics, LLC")]
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2015")]
