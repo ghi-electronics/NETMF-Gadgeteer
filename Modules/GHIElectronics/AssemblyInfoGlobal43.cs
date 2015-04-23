b7513c8ad327cf91dc67e27e6a588d6901fb9e00
John <john.brochue@ghielectronics.com>
2015-04-23 08:40:10 -0400

    Commit for SDK release 2015.1.5.0

--- Modules/GHIElectronics/AssemblyInfoGlobal43.cs
+++ Modules/GHIElectronics/AssemblyInfoGlobal43.cs
@@ -2,8 +2,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("4.3.7.4")]
-[assembly: AssemblyFileVersion("4.3.7.4")]
+[assembly: AssemblyVersion("4.3.7.5")]
+[assembly: AssemblyFileVersion("4.3.7.5")]
 [assembly: AssemblyCompany("GHI Electronics, LLC")]
 [assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2015")]
 [assembly: AssemblyTrademark("")]
