09a5f0cda24076376330fd83c4059a61b8e9f2dd
John Brochue <john.brochue@ghielectronics.com>
2015-12-21 08:48:03 -0500

    Fixed result scaling in AccelG248 and preallocated the read buffers.

--- Modules/GHIElectronics/AccelG248/AccelG248_43/AccelG248_43.cs
+++ Modules/GHIElectronics/AccelG248/AccelG248_43/AccelG248_43.cs
@@ -5,7 +5,9 @@ namespace Gadgeteer.Modules.GHIElectronics {
 	/// <summary>An AccelG248 module for Microsoft .NET Gadgeteer</summary>
 	public class AccelG248 : GTM.Module {
 		private GTI.I2CBus i2c;
-		private byte[] buffer;
+		private byte[] buffer1;
+		private byte[] buffer2;
+		private byte[] buffer6;
 
 		/// <summary>Constructs a new instance.</summary>
 		/// <param name="socketNumber">The socket that this module is plugged in to.</param>
@@ -13,52 +15,55 @@ namespace Gadgeteer.Modules.GHIElectronics {
 			Socket socket = Socket.GetSocket(socketNumber, true, this, null);
 			socket.EnsureTypeIsSupported('I', this);
 
-			this.buffer = new byte[1];
+			this.buffer1 = new byte[1];
+			this.buffer2 = new byte[2];
+			this.buffer6 = new byte[6];
+
 			this.i2c = GTI.I2CBusFactory.Create(socket, 0x1C, 400, this);
 			this.i2c.Write(0x2A, 0x01);
 		}
 
 		/// <summary>Gets the current X acceleration value.</summary>
-		/// <returns>The X acceleration between -1 and 1.</returns>
+		/// <returns>The X acceleration between -2g and 2g.</returns>
 		public double GetX() {
-			var data = this.ReadRegister(0x01, 2);
+			this.ReadRegister(0x01, this.buffer2);
 
-			double value = (data[0] << 2) | (data[1] >> 6);
+			double value = (this.buffer2[0] << 2) | (this.buffer2[1] >> 6);
 
 			if (value > 511.0)
 				value = value - 1024.0;
 
-			value /= 512.0;
+			value /= 256.0;
 
 			return value;
 		}
 
 		/// <summary>Gets the current Y acceleration value.</summary>
-		/// <returns>The Y acceleration between -1 and 1.</returns>
+		/// <returns>The Y acceleration between -2g and 2g.</returns>
 		public double GetY() {
-			var data = this.ReadRegister(0x03, 2);
+			this.ReadRegister(0x03, this.buffer2);
 
-			double value = (data[0] << 2) | (data[1] >> 6);
+			double value = (this.buffer2[0] << 2) | (this.buffer2[1] >> 6);
 
 			if (value > 511.0)
 				value = value - 1024.0;
 
-			value /= 512.0;
+			value /= 256.0;
 
 			return value;
 		}
 
 		/// <summary>Gets the current Z acceleration value.</summary>
-		/// <returns>The Z acceleration between -1 and 1.</returns>
+		/// <returns>The Z acceleration between -2g and 2g.</returns>
 		public double GetZ() {
-			var data = this.ReadRegister(0x05, 2);
+			this.ReadRegister(0x05, this.buffer2);
 
-			double value = (data[0] << 2) | (data[1] >> 6);
+			double value = (this.buffer2[0] << 2) | (this.buffer2[1] >> 6);
 
 			if (value > 511.0)
 				value = value - 1024.0;
 
-			value /= 512.0;
+			value /= 256.0;
 
 			return value;
 		}
@@ -74,15 +79,15 @@ namespace Gadgeteer.Modules.GHIElectronics {
 		}
 
 		/// <summary>Gets the current acceleration values.</summary>
-		/// <param name="x">The x acceleration between -1 and 1.</param>
-		/// <param name="y">The y acceleration between -1 and 1.</param>
-		/// <param name="z">The z acceleration between -1 and 1.</param>
+		/// <param name="x">The x acceleration between -2g and 2g.</param>
+		/// <param name="y">The y acceleration between -2g and 2g.</param>
+		/// <param name="z">The z acceleration between -2g and 2g.</param>
 		public void GetXYZ(out double x, out double y, out double z) {
-			var data = this.ReadRegister(0x01, 6);
+			this.ReadRegister(0x01, this.buffer6);
 
-			x = (data[0] << 2) | (data[1] >> 6);
-			y = (data[2] << 2) | (data[3] >> 6);
-			z = (data[4] << 2) | (data[5] >> 6);
+			x = (this.buffer6[0] << 2) | (this.buffer6[1] >> 6);
+			y = (this.buffer6[2] << 2) | (this.buffer6[3] >> 6);
+			z = (this.buffer6[4] << 2) | (this.buffer6[5] >> 6);
 
 			if (x > 511.0)
 				x -= 1024.0;
@@ -93,31 +98,26 @@ namespace Gadgeteer.Modules.GHIElectronics {
 			if (z > 511.0)
 				z -= 1024.0;
 
-			x /= 512.0;
-			y /= 512.0;
-			z /= 512.0;
+			x /= 256.0;
+			y /= 256.0;
+			z /= 256.0;
 		}
 
-		private byte[] ReadRegister(byte register, int count) {
-			byte[] result = new byte[count];
+		private void ReadRegister(byte register, byte[] read) {
+			this.buffer1[0] = register;
 
-			this.buffer[0] = register;
-
-			this.i2c.WriteRead(this.buffer, result);
-
-			return result;
+			this.i2c.WriteRead(this.buffer1, read);
 		}
 
 		/// <summary>Represents an acceleration.</summary>
 		public struct Acceleration {
-
-			/// <summary>The x acceleration between -1 and 1.</summary>
+			/// <summary>The x acceleration between -2g and 2g.</summary>
 			public double X { get; set; }
 
-			/// <summary>The y acceleration between -1 and 1.</summary>
+			/// <summary>The y acceleration between -2g and 2g.</summary>
 			public double Y { get; set; }
 
-			/// <summary>The z acceleration between -1 and 1.</summary>
+			/// <summary>The z acceleration between -2g and 2g.</summary>
 			public double Z { get; set; }
 
 			/// <summary>Returns the string representation of this object.</summary>
