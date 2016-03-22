3239f514a7fc72471f542a74e4398c5f2a7a9536
John Brochue <john.brochue@ghielectronics.com>
2016-03-22 12:05:40 -0400

    Updated copyright year.

--- Modules/GHIElectronics/AssemblyInfoGlobal43.cs
+++ Modules/GHIElectronics/AssemblyInfoGlobal43.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("4.3.8.0")]
 [assembly: AssemblyFileVersion("4.3.8.0")]
 [assembly: AssemblyCompany("GHI Electronics, LLC")]
-[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2015")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
